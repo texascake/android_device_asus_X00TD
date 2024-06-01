c54a4ac33d824c2d7aeb2e326e910318b2d653bb
Kneba <abenkenary3@gmail.com>
2024-06-01 15:39:17 +0000

    Revert "X00T: Add config for apps DANA"
    This reverts commit 8ec23f36aedd923db5ff245158a4e0ae42706464.
    
    - is no longer needed, signed build happy!

--- device.mk
+++ device.mk
@@ -116,10 +116,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# DANA
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/id.dana.xml:$(TARGET_COPY_OUT_DATA)/system/ifw/id.dana.xml
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
