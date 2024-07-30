19af243acbf2a6d9099ec42070f74ae2b704d312
dotkit <dotkit@electrowizard.me>
2024-07-30 16:26:32 +0000

    X00TD: Decommonize biometrics
    Change-Id: Ic98ad6782bc2b76f3f581e7446415c85a454deac
    Signed-off-by: dotkit <dotkit@electrowizard.me>

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-X00TD
+    android.hardware.biometrics.fingerprint@2.1-service.X00TD
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
