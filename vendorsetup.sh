7421d342916df2cebe6afc93cfb3e4b45cba9911
texascake <budalblimbing@gmail.com>
2024-09-16 22:28:55 +0700

    X00TD: Declare bootable_deprecated-ota

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,9 @@ rm -rf kernel/asus/sdm660/KernelSU/website
 rm -rf kernel/asus/sdm660/KernelSU/scripts
 rm -rf kernel/asus/sdm660/KernelSU/docs
 
+rm -rf bootable/deprecated-ota
+git clone git clone --depth=1 https://github.com/yaap/bootable_deprecated-ota bootable/deprecated-ota
+
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/proprietary_vendor_asus-4.19 vendor/asus
 
