af40bc2824e379ca5ab8b94529fd460fd17a7ba1
texascake <budalblimbing@gmail.com>
2024-10-12 22:14:33 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 rm -rf device/asus/sdm660-common
 rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/texascake/android_device_asus_sdm660-common -b evos device/asus/sdm660-common
-git clone --depth=1 https://github.com/texascake/android_device_asus_X00TD -b sakura device/asus/X00TD
+git clone --depth=1 https://github.com/texascake/android_device_asus_X00TD -b afl device/asus/X00TD
 
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b main kernel/asus/sdm660
@@ -13,7 +13,7 @@ rm -rf kernel/asus/sdm660/KernelSU/docs
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/texascake/proprietary_vendor_asus-4.19 vendor/asus
 
-git clone https://github.com/inexdroid/keys sig && cp -R sig/* vendor/sakura-priv/ && rm -rf sig
+git clone https://github.com/inexdroid/keys sig && cp -R sig/* vendor/after-priv/ && rm -rf sig
 
 export KBUILD_BUILD_USER=queen
 export TZ=Asia/Jakarta
