3266084dc27e4512e39d97b24c4578cda292babc
texascake <budalblimbing@gmail.com>
2024-10-13 23:02:57 +0700

    X00T: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,8 @@ rm -rf hardware/qcom-caf/msm8998/audio
 git clone --depth=1 https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
 
 # Build
-rm -rf build/make
-git clone --depth=1 https://github.com/lineageX00T/nad_build -b 10 build/make
+#rm -rf build/make
+#git clone --depth=1 https://github.com/lineageX00T/nad_build -b 10 build/make
 
 # Device
 rm -rf device/asus/X00T
@@ -13,8 +13,8 @@ rm -rf device/asus/sdm660-common
 git clone --depth=1 https://github.com/texascake/android_device_asus_X00TD -b nad device/asus/X00T
 
 # Frameworks
-rm -rf frameworks/base
-git clone --depth=1 https://github.com/lineageX00T/frameworks_base -b 10 frameworks/base
+#rm -rf frameworks/base
+#git clone --depth=1 https://github.com/lineageX00T/frameworks_base -b 10 frameworks/base
 
 # Kernel
 rm -rf kernel/asus/sdm660
@@ -29,20 +29,20 @@ rm -rf hardware/lineage/livedisplay
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_livedisplay -b lineage-17.1 hardware/lineage/livedisplay
 
 # Skia
-rm -rf external/skia
-git clone --depth=1 https://github.com/LineageOS/android_external_skia -b lineage-17.1 external/skia
+#rm -rf external/skia
+#git clone --depth=1 https://github.com/LineageOS/android_external_skia -b lineage-17.1 external/skia
 
 # Syetem core
-rm -rf system/core
-git clone --depth=1 https://github.com/lineageX00T/android_system_core -b 10 system/core
+#rm -rf system/core
+#git clone --depth=1 https://github.com/lineageX00T/android_system_core -b 10 system/core
 
 # Vendor
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/vendor_asus -b lineage-17.1 vendor/asus
 
 # Vendor nusantara
-rm -rf vendor/nusantara
-git clone --depth=1 https://github.com/lineageX00T/android_vendor_nusantara -b 10 vendor/nusantara
+#rm -rf vendor/nusantara
+#git clone --depth=1 https://github.com/lineageX00T/android_vendor_nusantara -b 10 vendor/nusantara
 
 # Vendor sign
 rm -rf vendor/lineage-priv
